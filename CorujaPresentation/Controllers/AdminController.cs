2a715d0b83a60ff06ecbc51f92b01bdefd9e3ba6
tomasrangel75 <tomas.rangel75@gmail.com>
2016-09-06 18:52:00 -0300

    20160906 18:51

--- CorujaPresentation/Controllers/AdminController.cs
+++ CorujaPresentation/Controllers/AdminController.cs
@@ -37,92 +37,167 @@ namespace CorujaPresentation.Controllers
         }
        
         [HttpPost]
-        public ActionResult CreateRole(string newwRoleName)
+        public ActionResult CreateRole(string Name)
         {
-            var _newRoleName = newwRoleName;
-            if (ModelState.IsValid)
-            {
 
-                if (roleManager.RoleExists(_newRoleName) == false)
+            try
+            {
+                var _newRoleName = Name;
+
+                if (roleManager.RoleExists(_newRoleName) )
                 {
                     var role = new IdentityRole(_newRoleName);
                     roleManager.Create(role);
                 }
                 db.SaveChanges();
-
+                TempData["ResultMessage"] = "Perfil criado com sucesso!";
+        
             }
+            catch (Exception exc)
+            {
+                TempData["ResultMessage"] = exc.Message.ToString();
+            }
+      
             return RedirectToAction("Roles");
         }
 
         public ActionResult DeleteRole(string id)
         {
-            if (id == null)
+            try
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                var role = roleManager.FindById(id);
+                roleManager.Delete(role);
+                db.SaveChanges();
+                TempData["ResultMessage"] = "Perfil excluído com sucesso!";
             }
-            var role = roleManager.FindById(id);
-            if (role == null)
+            catch (Exception exc)
             {
-                return HttpNotFound();
-            }
-            return View(role);
-        }
+                TempData["ResultMessage"] = exc.Message.ToString();
 
-        [HttpPost, ActionName("DeleteRole")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(string id)
-        {
-            var role = roleManager.FindById(id);
-            roleManager.Delete(role);
-            db.SaveChanges();
+            }
+           
             return RedirectToAction("Roles");
         }
 
-        /// //////////////////////////////////////////////////////////////
-
-        public ActionResult ListOfUsers(int id)
+        public ActionResult ListOfUsers(string roleName)
         {
 
+            var role = db.Roles.Where(r => r.Name.Equals(roleName));
+            var roleId = role.First().Id;
+            var allUsers =
+                       from roleUsers in db.Users
+                       orderby roleUsers.FirstName, roleUsers.LastName
+                       where roleUsers.Roles.Any(r => r.RoleId.Equals(roleId))
+                       select new RoleUsers { UserMail = roleUsers.Email, UserName = roleUsers.FirstName + " " + roleUsers.LastName };
 
-            return View();
+            return View(allUsers);
+
+        } 
+        
+        // Usuarios ////////////////////////////////////////
+
+        public ActionResult SysUsers()
+        {
+            var allUsers = db.Users.ToList();
+            return View(allUsers);
+
         }
 
-        //[HttpPost]
-        //	public ActionResult ListOfUsersAction(string id)
-        //	{
-        //			var role = new IdentityRole();
-        //		var users = role.Users.ToList();
+        public ActionResult SysUserDetails(string userEmail)
+        {
+            var selectedUser = db.Users.Select(x => x.Email.Equals(userEmail));
 
-
-        //		RedirectToAction("RoleManager");
-        //	}
-
-
+            return View(selectedUser);
+        }
 
         //Usuários e Perfis ////////////////////////////////
 
-        [ActionName("DeleteUser")]
-        public ActionResult DeleteUserFromRole(string id)
+        public ActionResult DefineUserRoles()
         {
+            // prepopulat roles for the view dropdown
+            var lst = db.Roles.OrderBy(r => r.Name).ToList().Select(rl =>
+            new SelectListItem { Value = rl.Name.ToString(), Text = rl.Name }).ToList();
+            ViewBag.Roles = lst;
+
             return View();
+
         }
 
-        [ActionName("DeleteUser")]
+
         [HttpPost]
-        public ActionResult DeleteUserFromRoleConfirm(string id)
+        [ValidateAntiForgeryToken]
+        public ActionResult RoleAddToUser(string UserName, string RoleName)
         {
-            return View();
+            ApplicationUser user = db.Users.Where(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            var account = new ContaController();
+            account.UserManager.AddToRole(user.Id, RoleName);
+
+            ViewBag.ResultMessage = "Perfil criado com sucesso";
+
+            // prepopulat roles for the view dropdown
+            var list = db.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+            ViewBag.Roles = list;
+
+            return View("DefineUserRoles");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult GetRoles(string UserName)
+        {
+            if (!string.IsNullOrWhiteSpace(UserName))
+            {
+                ApplicationUser user = db.Users.Where(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+                var account = new ContaController();
+
+                ViewBag.RolesForThisUser = account.UserManager.GetRoles(user.Id);
+
+                // prepopulat roles for the view dropdown
+                var list = db.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+                ViewBag.Roles = list;
+            }
+
+            return View("DefineUserRoles");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteRoleForUser(string UserName, string RoleName)
+        {
+            var account = new ContaController();
+            ApplicationUser user = db.Users.Where(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+
+            if (account.UserManager.IsInRole(user.Id, RoleName))
+            {
+                account.UserManager.RemoveFromRole(user.Id, RoleName);
+                ViewBag.ResultMessage = "Perfil removido com successo";
+            }
+            else
+            {
+                ViewBag.ResultMessage = "Esse usuário não pertence ao perfil selecionado";
+            }
+            // prepopulat roles for the view dropdown
+            var list = db.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+            ViewBag.Roles = list;
+
+            return View("DefineUserRoles");
         }
 
 
+       
+
+        public ActionResult ExcludeFromRole(string id)
+        {
+            return View();
+        }
+        
         public ActionResult AddUserToRole(string id)
         {
             ViewBag.Name = new SelectList(roleManager.Roles.ToList(), "Name", "Name");
             var roles = roleManager.Roles.ToList();
             return View(roles);
         }
-
-
+       
 
         protected override void Dispose(bool disposing)
         {
