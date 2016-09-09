152b826a9d5a623c42d369022c3f9aa2ebbcfe91
tomasrangel75 <tomas.rangel75@gmail.com>
2016-09-09 12:50:10 -0300

    20160909 12:50

--- CorujaPresentation/Models/AccountViewModels.cs
+++ CorujaPresentation/Models/AccountViewModels.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace CorujaPresentation.Models
 {
@@ -287,6 +288,26 @@ namespace CorujaPresentation.Models
         public string UserMail { get; set; }
         public string UserName { get; set; }
         public System.DateTime? RegisterDate { get; set; }
+    }
+
+    public class RoleViewModel
+    {
+        public string Id { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "Nome")]
+        public string Name { get; set; }
+    }
+
+    public class EditUserViewModel
+    {
+        public string Id { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public IEnumerable<SelectListItem> RolesList { get; set; }
     }
 
 }
