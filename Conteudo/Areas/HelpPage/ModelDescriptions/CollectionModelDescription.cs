d5c041c5ba9ce23c2c4d31868c13fbc7aa84c392
tomasrangel75 <tomas.rangel75@gmail.com>
2016-10-11 13:01:01 -0300

    13:00

--- Conteudo/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Conteudo.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
